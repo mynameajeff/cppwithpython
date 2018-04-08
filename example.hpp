753831a857711f129074b9f53ea388aed07b05e1
Lorenz Schymanietz <yungcoder@protonmail.com>
2018-04-08 13:55:59 +0100

    Create example.hpp

---
+++ example.hpp
@@ -0,0 +1,9 @@
+
+// Headers:
+#include <boost/python.hpp>
+#include <iostream>
+//~~~~~~~~~~~~~~~~~~~~~
+
+// Function Prototypes:
+template <typename T> const T power(T base, T exponent);
+//~~~~~~~~~~~~~~~~~~~~~
